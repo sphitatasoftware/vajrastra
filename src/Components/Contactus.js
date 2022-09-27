3fee6cbff001d18cec5deb3444f9545accdc1930
= <chrishotelsoftware@gmail.com>
2022-09-27 23:47:52 +0530

    new

--- src/Components/Contactus.js
+++ src/Components/Contactus.js
@@ -8,15 +8,14 @@ export default Contactus => {
         <h1>Get in touch and let's make something great together.</h1>
         <br/>
         <br/>
-        <h2>sales@asthra.com</h2>
+        <h2>sales@vajrasthra.com</h2>
         <h3> +91 8586987306</h3>
         <br/>
-        <br/>
         <h4>WHERE TO FIND US</h4>
-        <h5>29/500, 1st Floor, Oasis Plaza,<br/>
-Madhurwada,<br/>
-Visakhapatnam, Andhra Pradesh,<br/>
-India - 531 023</h5>
+        <h5>29/500, 1st Floor, Oasis Plaza,
+Madhurwada,
+Visakhapatnam, Andhra Pradesh,
+India - 531 023</h5><br/>
       
     
 </div>
